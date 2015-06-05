a76b6e2adb92206d633315076784830d2f5b35ce
mrmiguez <r.m.miguez@gmail.com>
2015-06-05 19:26:28 -0400

    arguments for input and output

--- xmlCSVout.py
+++ xmlCSVout.py
@@ -1,13 +1,21 @@
 import xml.etree.ElementTree as ET
 import csv
+import sys
 
-NS = 'http://www.tei-c.org/ns/1.0'
-with open('nileMeasure.csv', 'w') as f:
-	writer = csv.writer(f)
-	tree = ET.parse('nileMeasure.xml')
-	root = tree.getroot()
-	for row in root.iterfind('.//{%s}row' % NS ):
-		data = []
-		for cell in row.iterfind('./{%s}cell' % NS ):
-			data.append(cell.text)
-		writer.writerow(data)
+def writeCSV(fileIn, fileOut):
+#	print(fileIn)
+#	print(fileOut)
+	NS = 'http://www.tei-c.org/ns/1.0'
+	with open(fileOut, 'w') as f:
+		writer = csv.writer(f)
+		tree = ET.parse(fileIn)
+		root = tree.getroot()
+		for row in root.iterfind('.//{%s}row' % NS ):
+			data = []
+			for cell in row.iterfind('./{%s}cell' % NS ):
+				data.append(cell.text)
+			writer.writerow(data)
+
+fileIn = sys.argv[1]
+fileOut = sys.argv[2]
+writeCSV(fileIn, fileOut)
